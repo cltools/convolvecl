476347e20146b9b3e3b588cc6b5e0814e98858ef
Nicolas Tessore <n.tessore@ucl.ac.uk>
2022-04-04 14:55:10 +0100

    support for multiple axes

--- convolvecl/_mixmat.py
+++ convolvecl/_mixmat.py
@@ -9,56 +9,64 @@ FOUR_PI = 4*np.pi
 
 @njit(nogil=True, parallel=True, fastmath=True, cache=True)
 def _mixmat_full(m, v, l1min, l2min, l1max, l2max, s1, s2, s1_, s2_):
+    ncl = m.shape[:-2]
     for l1 in prange(l1min, l1max+1):
         for l2 in prange(l2min, l2max+1):
             buf = np.empty(l1max+l2max+1)
             buf_ = np.empty(l1max+l2max+1)
-            a, c = threejj(l1, l2, s1, -s2, out=buf)
-            _, c_ = threejj(l1, l2, s1_, -s2_, out=buf_)
-            t = 0.
-            for x, y, z in zip(c, c_, v[int(a):]):
-                t += x*y*z
-            m[l1, l2] = (2*l2+1)*t
+            l3min, thrcof = threejj(l1, l2, s1, -s2, out=buf)
+            _, thrcof_ = threejj(l1, l2, s1_, -s2_, out=buf_)
+            for i in np.ndindex(*ncl):
+                t = 0.
+                for x, y, z in zip(thrcof, thrcof_, v[i][int(l3min+0.01):]):
+                    t += x*y*z
+                m[i][l1, l2] = (2*l2+1)*t
 
 
 @njit(nogil=True, parallel=True, fastmath=True, cache=True)
 def _mixmat_full_same(m, v, l1min, l2min, l1max, l2max, s1, s2):
+    ncl = m.shape[:-2]
     for l1 in prange(l1min, l1max+1):
         for l2 in prange(l2min, l2max+1):
             buf = np.empty(l1max+l2max+1)
-            a, c = threejj(l1, l2, s1, -s2, out=buf)
-            t = 0.
-            for x, z in zip(c, v[int(a):]):
-                t += x*x*z
-            m[l1, l2] = (2*l2+1)*t
+            l3min, thrcof = threejj(l1, l2, s1, -s2, out=buf)
+            for i in np.ndindex(*ncl):
+                t = 0.
+                for x, z in zip(thrcof, v[i][int(l3min+0.01):]):
+                    t += x*x*z
+                m[i][l1, l2] = (2*l2+1)*t
 
 
 @njit(nogil=True, parallel=True, fastmath=True, cache=True)
 def _mixmat_symm(m, v, lmin, lmax, s1, s2, s1_, s2_):
+    ncl = m.shape[:-2]
     for l1 in prange(lmin, lmax+1):
         for l2 in prange(l1, lmax+1):
             buf = np.empty(2*lmax+1)
             buf_ = np.empty(2*lmax+1)
-            a, c = threejj(l1, l2, s1, -s2, out=buf)
-            _, c_ = threejj(l1, l2, s1_, -s2_, out=buf_)
-            t = 0.
-            for x, y, z in zip(c, c_, v[int(a):]):
-                t += x*y*z
-            m[l1, l2] = (2*l2+1)*t
-            m[l2, l1] = (2*l1+1)*t
+            l3min, thrcof = threejj(l1, l2, s1, -s2, out=buf)
+            _, thrcof_ = threejj(l1, l2, s1_, -s2_, out=buf_)
+            for i in np.ndindex(*ncl):
+                t = 0.
+                for x, y, z in zip(thrcof, thrcof_, v[i][int(l3min+0.01):]):
+                    t += x*y*z
+                m[i][l1, l2] = (2*l2+1)*t
+                m[i][l2, l1] = (2*l1+1)*t
 
 
 @njit(nogil=True, parallel=True, fastmath=True, cache=True)
 def _mixmat_symm_same(m, v, lmin, lmax, s1, s2):
+    ncl = m.shape[:-2]
     for l1 in prange(lmin, lmax+1):
         for l2 in prange(l1, lmax+1):
             buf = np.empty(2*lmax+1)
-            a, c = threejj(l1, l2, s1, -s2, out=buf)
-            t = 0.
-            for x, z in zip(c, v[int(a):]):
-                t += x*x*z
-            m[l1, l2] = (2*l2+1)*t
-            m[l2, l1] = (2*l1+1)*t
+            l3min, thrcof = threejj(l1, l2, s1, -s2, out=buf)
+            for i in np.ndindex(*ncl):
+                t = 0.
+                for x, z in zip(thrcof, v[i][int(l3min+0.01):]):
+                    t += x*x*z
+                m[i][l1, l2] = (2*l2+1)*t
+                m[i][l2, l1] = (2*l1+1)*t
 
 
 def mixmat(cl, l1max=None, l2max=None, l3max=None, spin=(0, 0), spin_out=None):
@@ -94,13 +102,13 @@ def mixmat(cl, l1max=None, l2max=None, l3max=None, spin=(0, 0), spin_out=None):
     '''
 
     if l1max is None:
-        l1max = len(cl)-1
+        l1max = n-1
 
     if l2max is None:
         l2max = l1max
 
-    if l3max is None or l3max > len(cl)-1:
-        l3max = len(cl)-1
+    if l3max is None or l3max > n-1:
+        l3max = n-1
 
     if spin_out is None:
         spin_out = spin
@@ -113,8 +121,8 @@ def mixmat(cl, l1max=None, l2max=None, l3max=None, spin=(0, 0), spin_out=None):
     same_spins = (s1 == s1_) and (s2 == s2_)
     symmetric = (s1 == s2) and (s1_ == s2_)
 
-    m = np.zeros((l1max+1, l2max+1))
-    v = np.arange(1, 2*l3max+2, 2)*cl[:l3max+1]/FOUR_PI
+    m = np.zeros((*ncl, l1max+1, l2max+1))
+    v = np.arange(1, 2*l3max+2, 2)*cl[..., :l3max+1]/FOUR_PI
 
     if symmetric:
         lmin, lmax = max(l1min, l2min), min(l1max, l2max)
